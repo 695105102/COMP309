5e55d126068de499eab1120d1b56ca20b18bfef3
695105102 <toantin.sogiaoduclamdong@gmail.com>
2023-02-25 16:06:35 +0700

    fist commit

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include "code.h"
+void sap_xep_day_so(int* a, int n)
+{
+		//ma lenh sap xep day so
+}
+int. tim_main(int* a, int n)
+{
+		// ma lenh ham Tim max
+		
+}
+int. tim_main()
+{
+		cout << "Phien ban 2";
+		return 0;
+}
---
+++ code.h
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+
+void sap_xep_day_so(int* a, int n);
+int tim max(int* a, int n);
