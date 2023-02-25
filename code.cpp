a3e480e9ba1f6b5ac6bb15b261b70eb4990a753e
695105102 <toantin.sogiaoduclamdong@gmail.com>
2023-02-25 20:42:17 +0700

    my function 1

--- code.cpp
+++ code.cpp
@@ -3,13 +3,18 @@ void sap_xep_day_so(int* a, int n)
 {
 		//ma lenh sap xep day so
 }
-int. tim_main(int* a, int n)
+int. tim_max(int* a, int n)
 {
 		// ma lenh ham Tim max
 		
 }
+int. tim_min(int* a, int n)
+{
+		// ma lenh ham Tim min
+		
+}
 int. tim_main()
 {
-		cout << "Phien ban 2";
+		cout << "my function";
 		return 0;
 }
